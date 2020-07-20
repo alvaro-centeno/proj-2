8ecba1b2eee42e15485dddd03bf493ebcb7f3cc7
Alvaro Centeno <alvaroc0689@gmail.com>
2020-07-20 16:10:21 -0700

    updated sunlight and enoughplants

--- client/assets/rooms.js
+++ client/assets/rooms.js
@@ -1,19 +1,11 @@
 $(document).ready(function () {
 
-    // getRooms().then((allRooms) => {
-    //     renderRooms(allRooms);
-    // });
-
-    // const name = $('#roomName');
-    // const size = $('#roomSize');
-    // const numPlant = $('#plantNum');
-
     let clicked = false;
 
     $('#start').on('click', function () {
         if (clicked === false) {
-        $('#questions').append(
-            `<div class="row">
+            $('#questions').append(
+                `<div class="row">
         <div class="col s12 m6">
             <div class="card green lighten-5">
                 <div class="card-content white-text">
@@ -74,13 +66,13 @@ $(document).ready(function () {
                     <form action="#">
                         <p>
                             <label>
-                                <input type="checkbox" id="opt1">
+                                <input type="checkbox" id="opt1" value = true>
                                 <span>yes</span>
                             </label>
                         </p>
                         <p>
                             <label>
-                                <input type="checkbox" id="opt2">
+                                <input type="checkbox" id="opt2" value = false >
                                 <span>no</span>
                             </label>
                         </p>
@@ -96,49 +88,63 @@ $(document).ready(function () {
         <i class="material-icons right"></i>
     </button>
        `
-    );
-}
-clicked = true;
-    $("#btnSubmit").on("click", function () {
-      function getInputValue() {
-        const roomText = $("#roomName").val();
-        $("#roomName").val("");
-
-        let roomSize = document.getElementById("roomSize").value;
-        let plantNum = document.getElementById("plantNum").value;
-        console.log(roomText, roomSize, plantNum);
-        if (roomSize <= 100 && plantNum <= 1) {
-          console.log("small room");
-        } else if (
-          roomSize > 101 &&
-          roomSize < 300 &&
-          plantNum >= 1 &&
-          plantNum <= 3
-        ) {
-          console.log("medium room + 2 plants");
-        } else if (roomSize > 301 && plantNum >= 3) {
-          console.log("large room");
+            );
         }
+        clicked = true;
+        $("#btnSubmit").on("click", function () {
+            function getInputValue() {
+                const roomText = $("#roomName").val();
+                $("#roomName").val("");
 
-        
-        $.ajax({
-          type: "POST",
-          url: "/room/new",
-          data: {
-            name: roomText,
-            size: roomSize,
-            numPlants: plantNum,
-            sunlight: true,
-          },
-        }).then((response) => {
-          $("#questions").empty();
-          console.log(response);
-          // getRooms()
-          //     .then((allRooms) => renderRooms(allRooms))
-          //     .catch((err) => console.log(err));
+                let roomSize = document.getElementById("roomSize").value;
+                let plantNum = document.getElementById("plantNum").value;
+                let opt1 = document.getElementById('opt1').value;
+                let opt2 = document.getElementById('opt2').value;
+                let sunlight = true;
+                let enough = true;
+                console.log(roomText, roomSize, plantNum);
+                if (roomSize <= 100 && plantNum <= 1) {
+                    console.log("small room");
+                    enough = false;
+                } else if (
+                    roomSize > 101 &&
+                    roomSize < 300 &&
+                    plantNum >= 1 &&
+                    plantNum <= 3
+                ) {
+                    console.log("medium room + 2 plants");
+                    enough = false;
+                } else if (roomSize > 301 && plantNum >= 3) {
+                    console.log("large room");
+                    enough = true;
+                }
 
-          $("#newRoom").append(
-            `<div class="col s12 m2">
+                if (opt1) {
+                    sunlight = true;
+                } else {
+                    sunlight = false;
+                }
+
+
+                $.ajax({
+                    type: "POST",
+                    url: "/room/new",
+                    data: {
+                        name: roomText,
+                        size: roomSize,
+                        numPlants: plantNum,
+                        enoughPlants: enough,
+                        sunlight: sunlight,
+                    },
+                }).then((response) => {
+                    $("#questions").empty();
+                    console.log(response);
+                    // getRooms()
+                    //     .then((allRooms) => renderRooms(allRooms))
+                    //     .catch((err) => console.log(err));
+
+                    $("#newRoom").append(
+                        `<div class="col s12 m2">
                     <div class="card horizontal">
                         <div class="card-image">
                         <img src="https://cdn.vox-cdn.com/thumbor/QRC-K6S73KSM0XVNvrQhicj9g_E=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/65377475/BT7B0071.7.jpg">
@@ -158,50 +164,50 @@ clicked = true;
                     </div>`
 
 
-                    
-          );
+
+                    );
+                });
+            }
+            getInputValue();
         });
-      }
-      getInputValue();
     });
-  });
 
-  // Button to remove room
-$("#newRoom").on("click", ".buttondelete", deleteRoom);
-
-
-  const getRooms = () => {
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        type: "GET",
-        url: "/rooms",
-      })
-        .then((res) => {
-          resolve(res);
-        })
-        .catch((err) => reject(err));
-    });
-  };
+    // Button to remove room
+    $("#newRoom").on("click", ".buttondelete", deleteRoom);
 
 
-// var random_images_array = ["pic1.png", "pic2.png", "pic3.png", "pic4.png", "BT7B0071.png"];
+    const getRooms = () => {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: "GET",
+                url: "/rooms",
+            })
+                .then((res) => {
+                    resolve(res);
+                })
+                .catch((err) => reject(err));
+        });
+    };
 
-//         function getRandomImage(imgAr, path) {
-//             // path ='./pictures/'; // default path here
-//             var num = Math.floor( Math.random() * imgAr.length );
-//             var img = imgAr[ num ];
-//             var imgStr = '<img src="' + path + img + '" alt = "">';
-//             document.write(imgStr); document.close();
-//         }
+
+    // var random_images_array = ["pic1.png", "pic2.png", "pic3.png", "pic4.png", "BT7B0071.png"];
+
+    //         function getRandomImage(imgAr, path) {
+    //             // path ='./pictures/'; // default path here
+    //             var num = Math.floor( Math.random() * imgAr.length );
+    //             var img = imgAr[ num ];
+    //             var imgStr = '<img src="' + path + img + '" alt = "">';
+    //             document.write(imgStr); document.close();
+    //         }
 
 
 
-  getRooms().then((res) => {
-    console.log(res);
-    res.forEach((room) => {
-        $("#newRoom").empty();
-      $("#newRoom").append(
-        `<div class="col s12 m2">
+    getRooms().then((res) => {
+        console.log(res);
+        res.forEach((room) => {
+            $("#newRoom").empty();
+            $("#newRoom").append(
+                `<div class="col s12 m2">
           <div class="card horizontal">
               <div class="card-image">
                   <img src="https://cdn.vox-cdn.com/thumbor/QRC-K6S73KSM0XVNvrQhicj9g_E=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/65377475/BT7B0071.7.jpg">
@@ -219,25 +225,25 @@ $("#newRoom").on("click", ".buttondelete", deleteRoom);
               </div>
               </div>
           </div>`
-      );
+            );
+        });
     });
-  });
 
-  //function to delete a room
-  function deleteRoom(event) {
-    console.log("clicked");
-    event.stopPropagation();
-    var id = $(this).data("id");
-    $.ajax({
-      method: "DELETE",
-      url: "/room/delete/" + id,
-    }).then((res) =>{
-        getRooms().then((res) => {
-            console.log(res);
-            res.forEach((room) => {
-                $("#newRoom").empty();
-              $("#newRoom").append(
-                `<div class="col s12 m2">
+    //function to delete a room
+    function deleteRoom(event) {
+        console.log("clicked");
+        event.stopPropagation();
+        var id = $(this).data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/room/delete/" + id,
+        }).then((res) => {
+            getRooms().then((res) => {
+                console.log(res);
+                res.forEach((room) => {
+                    $("#newRoom").empty();
+                    $("#newRoom").append(
+                        `<div class="col s12 m2">
                   <div class="card horizontal">
                       <div class="card-image">
                           <img src="https://cdn.vox-cdn.com/thumbor/QRC-K6S73KSM0XVNvrQhicj9g_E=/0x0:2000x1333/1200x800/filters:focal(840x507:1160x827)/cdn.vox-cdn.com/uploads/chorus_image/image/65377475/BT7B0071.7.jpg">
@@ -255,9 +261,9 @@ $("#newRoom").on("click", ".buttondelete", deleteRoom);
                       </div>
                       </div>
                   </div>`
-              );
+                    );
+                });
             });
-          });
-    });
-  }
+        });
+    }
 });
